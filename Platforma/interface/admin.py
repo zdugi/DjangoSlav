329c27d36780953270b099b12d1f0809d6c48f73
roki021 <rodusekvladimir@yahoo.com>
2017-03-14 23:02:06 +0100

    Koncacno sredjeno admin strana dodavanja eksperimenata

--- Platforma/interface/admin.py
+++ Platforma/interface/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from .models import Experiment
+from models import Experiment
 
 class ExperimentAdmin(admin.ModelAdmin):
-    list_display = ('naziv', 'adresa', 'datum_kreiranja')
-    readonly_fields = ('apikey', 'broj_pregleda',)
-       
+    list_display = ( 'naziv', 'apikey', 'adresa', 'datum_kreiranja')
+    
+    def get_form(self, request, obj=None, **kwargs):
+        if obj: # ako je obj not none onda je edit strana
+            kwargs['exclude'] = ['apikey', 'broj_pregleda',]
+        else: # ako je obj none onda je add strana
+            kwargs['fields'] = ['naziv', 'adresa', 'port', 'opis', 'datum_kreiranja', 'demo_video']
+        return super(ExperimentAdmin, self).get_form(request, obj, **kwargs)
+    
+    # ovo sam stavio jer readonly polja ne mogu da se sakriju
+    # pa sam preko ove funkcije ispitivao
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # ako je obj not none onda je edit strana
+            return ['apikey', 'broj_pregleda',]
+        else: # ako je obj none onda je add strana
+            return []
+            
 admin.site.register(Experiment, ExperimentAdmin)
